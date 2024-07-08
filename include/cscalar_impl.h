9e3bb01cb2980e24a65b7f0f51c94228bc1b06ee
RadhikaG <radhikaghosal@gmail.com>
2024-07-07 23:43:42 -0400

    eigen ast: figured out constr destr assert issue, added implicit cast from expr to cscalar so somehow some array assignments work now

--- include/cscalar_impl.h
+++ include/cscalar_impl.h
@@ -2,14 +2,19 @@
 #define CSCALAR_IMPL_H
 #include "builder/dyn_var.h"
 #include "builder/static_var.h"
-#include <memory>
 
 namespace SpatialAlgebra {
+
+template <typename T>
+struct cscalar;
 
 // Base class for all expressions that can appear on the RHS of a =
 template <typename T>
 struct cscalar_expr {
   virtual const builder::dyn_var<T> get_value_at() const { return 0; }
+  operator cscalar<T>() const {
+    return get_value_at();
+  }
 };
 
 //template <typename T>
@@ -24,37 +29,36 @@ struct cscalar {
   builder::dyn_var<T> m_value;
 
   // compile time optimization fields
-  builder::static_var<int> is_constant = true;
-  builder::static_var<int> is_zero = true;
+  builder::static_var<int> is_constant = false;
+  builder::static_var<int> is_zero = false;
   builder::static_var<int> is_one = false;
   builder::static_var<T> constant_val = 0;
+
+  // Delete the default implementations of the operators
+  // and copy constructors
+  //cscalar(const cscalar& other) = delete;
 
   cscalar() {
   }
 
-  cscalar(builder::dyn_var<T>& dyn_val) : is_constant(false), is_zero(false), m_value(dyn_val) {}
+  cscalar(builder::dyn_var<T>& dyn_val) : is_constant(false), is_zero(false), is_one(false), m_value(dyn_val) {}
 
-  cscalar(const builder::dyn_var<T>& dyn_val) : is_constant(false), is_zero(false), m_value(dyn_val) {}
+  cscalar(const builder::dyn_var<T>& dyn_val) : is_constant(false), is_zero(false), is_one(false), m_value(dyn_val) {}
 
   cscalar(T value) : is_constant(true), constant_val(value) {
-    m_value = constant_val;
     if (value == 0)
       is_zero = true;
     else if (value == 1)
       is_one = true;
   }
 
-  // Delete the default implementations of the operators
-  // and copy constructors
-  //cscalar(const cscalar& other) = delete;
-
   // Assignment operator overloads
 
-  //void operator=(const cscalar_expr<T> & rhs) {
-  //  // need to trigger get_value_at calls for each sub-expr inside expr tree
-  //  // to materialize AST into code
-  //  m_value = rhs.get_value_at();
-  //}
+  void operator=(const cscalar_expr<T> & rhs) {
+    // need to trigger get_value_at calls for each sub-expr inside expr tree
+    // to materialize AST into code
+    m_value = rhs.get_value_at();
+  }
 
   void operator=(const T& value) {
     m_value = value;
@@ -66,79 +70,81 @@ struct cscalar {
       is_one = true;
   }
 
-  //void operator=(const cscalar& other) {
-  //  // this actually calls operator = for cscalar_expr<T> rhs
-  //  *this = cscalar_expr_leaf<T>(other);
-  //}
   void operator=(const cscalar& other) {
     // this actually calls operator = for cscalar_expr<T> rhs
-    if (other.is_constant)
-      this->m_value = other.constant_val;
-    else
-      this->m_value = other.m_value;
+    *this = cscalar_expr_leaf<T>(other);
   }
+  //void operator=(const cscalar& other) {
+  //  // this actually calls operator = for cscalar_expr<T> rhs
+  //  if (other.is_constant) {
+  //    *this = other.constant_val;
+  //  }
+  //  else
+  //    m_value = other.m_value;
+  //}
 
-  const cscalar& operator+(const cscalar& other) const {
-    if (is_constant && other.is_constant)
-      return *new cscalar(constant_val + other.constant_val);
-    if (is_zero)
-      return *new cscalar(other);
-    if (other.is_zero)
-      return *this;
+  //const cscalar& operator+(const cscalar& other) const {
+  //  if (is_constant && other.is_constant) {
+  //    return *new cscalar(constant_val + other.constant_val);
+  //  }
+  //  if (is_zero)
+  //    return *new cscalar(other.m_value);
+  //  if (other.is_zero)
+  //    return *this;
 
-    return *new cscalar(m_value + other.m_value);
-  }
+  //  return *new cscalar(m_value + other.m_value);
+  //}
 
-  const cscalar& operator-(const cscalar& other) const {
-    if (is_constant && other.is_constant)
-      return *new cscalar(constant_val - other.constant_val);
-    if (is_zero)
-      return *new cscalar(-other.m_value);
-    if (other.is_zero)
-      return *this;
+  //const cscalar& operator-(const cscalar& other) const {
+  //  if (is_constant && other.is_constant)
+  //    return *new cscalar(constant_val - other.constant_val);
+  //  if (is_zero)
+  //    return *new cscalar(-other.m_value);
+  //  if (other.is_zero)
+  //    return *this;
 
-    return *new cscalar(m_value - other.m_value);
-  }
+  //  return *new cscalar(m_value - other.m_value);
+  //}
 
-  const cscalar& operator*(const cscalar& other) const {
-    if (is_constant && other.is_constant)
-      return *new cscalar(constant_val * other.constant_val);
-    if (is_one)
-      return other;
-    if (other.is_one)
-      return *this;
+  //const cscalar& operator*(const cscalar& other) const {
+  //  if (is_constant && other.is_constant)
+  //    return *new cscalar(constant_val * other.constant_val);
+  //  if (is_one)
+  //    return other;
+  //  if (other.is_one)
+  //    return *this;
 
-    return *new cscalar(m_value * other.m_value);
-  }
+  //  return *new cscalar(m_value * other.m_value);
+  //}
 
-  const cscalar& operator/(const cscalar& other) const {
-    if (is_constant && other.is_constant)
-      return *new cscalar(constant_val / other.constant_val);
-    if (is_zero)
-      return *new cscalar(0);
-    //if (other.is_zero)
-    //  assert
-    if (other.is_one)
-      return *this;
+  //const cscalar& operator/(const cscalar& other) const {
+  //  if (is_constant && other.is_constant)
+  //    return *new cscalar(constant_val / other.constant_val);
+  //  if (is_zero)
+  //    return *new cscalar(0);
+  //  //if (other.is_zero)
+  //  //  assert
+  //  if (other.is_one)
+  //    return *this;
 
-    return *new cscalar(m_value / other.m_value);
-  }
+  //  return *new cscalar(m_value / other.m_value);
+  //}
 
-  void operator+=(const cscalar& other) {
-    *this = *this + other;
-  }
+  //void operator+=(const cscalar& other) {
+  //  *this = *this + other;
+  //}
 
-  void operator-=(const cscalar& other) {
-    *this = *this - other;
-  }
+  //void operator-=(const cscalar& other) {
+  //  *this = *this - other;
+  //}
 
-  void operator*=(const cscalar& other) {
-    *this = *this * other;
-  }
+  //void operator*=(const cscalar& other) {
+  //  *this = *this * other;
+  //}
 
-  void operator/=(const cscalar& other) {
-    *this = *this / other;
-  }
+  //void operator/=(const cscalar& other) {
+  //  *this = *this / other;
+  //}
 
   friend std::ostream& operator<<(std::ostream& os, const cscalar& obj) {
     if (obj.is_constant)
@@ -186,6 +192,21 @@ struct cscalar_expr_leaf: public cscalar_expr<T> {
 };
 
 // Non leaf RHS expr classes
+
+// Equality
+template <typename T>
+struct cscalar_expr_eq: public cscalar_expr<T> {
+  const struct cscalar_expr<T>& expr1;
+  const struct cscalar_expr<T>& expr2;
+
+  cscalar_expr_eq(const struct cscalar_expr<T>& expr1, const struct cscalar_expr<T>& expr2):
+    expr1(expr1), expr2(expr2) {
+  }
+
+  const builder::dyn_var<T> get_value_at() const {
+    return expr1.get_value_at() == expr2.get_value_at();
+  }
+};
 
 // Addition
 template <typename T>
--- samples/sample1.cpp
+++ samples/sample1.cpp
@@ -1,53 +1,90 @@
 #include "builder/dyn_var.h"
 #include "builder/forward_declarations.h"
 #include "Eigen/Dense"
+#include "builder/static_var.h"
 #include "cscalar.h"
 #include "cscalar_impl.h"
+
+using builder::dyn_var;
+using builder::static_var;
 
 //void forward_kinematics(builder::dyn_var<Eigen::> joint_angles) {
 //
 //}
 
-//void blah() {
-//  Eigen::MatrixXcsd a(2,2);
-//  a << 
-//      SpatialAlgebra::csed(1), SpatialAlgebra::csed(0),
-//      SpatialAlgebra::csed(0), SpatialAlgebra::csed(1);
-//
-//  Eigen::MatrixXcsd b(2,2);
-//  b << 
-//      SpatialAlgebra::csed(0), SpatialAlgebra::csed(6),
-//      SpatialAlgebra::csed(7), SpatialAlgebra::csed(8);
-//
-//  Eigen::MatrixXcsd c(2,2);
-//
-//  c = a * b;
-//}
+void eep() {
+  Eigen::MatrixXcsd a(2,2);
+  a << 
+      1, 0,
+      0, 1;
+
+  Eigen::MatrixXcsd b(2,2);
+  b << 
+      0, 6,
+      7, 8;
+
+  Eigen::MatrixXcsd c(2,2);
+
+  //c = (a + b).eval();
+  c = (a + b).eval();
+  //c(0,0) = a(0,0) + b(0,0);
+  //c(1,0) = a(1,0) + b(1,0);
+  //c(0,1) = a(0,1) + b(0,1);
+  //c(1,1) = a(1,1) + b(1,1);
+}
+
+void meh() {
+  //SpatialAlgebra::cscalard a[4], b[4], c[4];
+  //SpatialAlgebra::cscalard sum = 0;
+
+  //for (static_var<int> i = 0; i < 2; i = i+1) {
+  //  for (static_var<int> j = 0; j < 2; j = j+1) {
+  //    sum = 0;
+  //    for (static_var<int> k = 0; k < 2; k = k+1) {
+  //      sum += a[i*2+k] * b[k*2+j];
+  //    }
+  //    c[i*2+j] = sum;
+  //  }
+  //}
+  SpatialAlgebra::cscalard a, b;
+  a = 2;
+  b = 3;
+  SpatialAlgebra::cscalard sum = 0;
+  sum += a; 
+  sum += b; 
+}
+
+void ree() {
+  std::vector<dyn_var<double>*> a, b, c;
+
+  for (static_var<int> i = 0; i < 4; i = i+1) {
+    dyn_var<double>& l = *new dyn_var<double>(i);
+    dyn_var<double>& p = *new dyn_var<double>(i+2);
+    dyn_var<double>& q = *new dyn_var<double>;
+    a.push_back(l.addr());
+    b.push_back(p.addr());
+    c.push_back(q.addr());
+  }
+
+  for (static_var<int> i = 0; i < 4; i = i+1) {
+    *c[i] = *a[i] + *b[i];
+  }
+}
 
 void blah() {
-  //Eigen::MatrixXcsd a(2,2);
-  //a <<
-  //    SpatialAlgebra::cscalard(1), SpatialAlgebra::cscalard(2),
-  //    SpatialAlgebra::cscalard(3), SpatialAlgebra::cscalard(4);
-
-  //Eigen::MatrixXcsd b(2,2);
-  //b <<
-  //    SpatialAlgebra::cscalard(5), SpatialAlgebra::cscalard(6),
-  //    SpatialAlgebra::cscalard(7), SpatialAlgebra::cscalard(8);
-
-  //Eigen::MatrixXcsd c(2,2);
-
-  //c = a + b;
-
   SpatialAlgebra::cscalard a(1);
   SpatialAlgebra::cscalard b(3);
-
-  SpatialAlgebra::cscalard x,y,z;
+  SpatialAlgebra::cscalard x;
 
   x = a + b;
-  //x = (a + b);// * b;
-  //x = (a + b) * b;
-  //x *= b;
+}
+
+void oof() {
+  dyn_var<double> a(1);
+  dyn_var<double> b(3);
+  dyn_var<double> x;
+
+  x = a + b;
 }
 
 int main(int argc, char* argv[]) {
@@ -55,6 +92,10 @@ int main(int argc, char* argv[]) {
     // do this at runtime because compile-time XML parsing
     // not required.
     
-	SpatialAlgebra::generate_spatial_algebra_program(blah, "blah", std::cout);
+	//SpatialAlgebra::generate_spatial_algebra_program(eep, "eep", std::cout);
+	SpatialAlgebra::generate_spatial_algebra_program(meh, "meh", std::cout);
+	//SpatialAlgebra::generate_spatial_algebra_program(ree, "ree", std::cout);
+	//SpatialAlgebra::generate_spatial_algebra_program(blah, "blah", std::cout);
+	//SpatialAlgebra::generate_spatial_algebra_program(oof, "oof", std::cout);
 	return 0;
 }
