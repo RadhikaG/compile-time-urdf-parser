92def29ab9fff4964c76c05f45aab04b24361643
Marc <lopez.domenech.marc@gmail.com>
2024-11-22 14:55:52 -0500

    New branch for isolating ctup from pinocchio

--- make/rules.mk
+++ make/rules.mk
@@ -1,6 +1,5 @@
 .PRECIOUS: $(BUILD_DIR)/%.o 
 .PRECIOUS: $(BUILD_DIR)/samples/%.o 
-.PRECIOUS: $(BUILD_DIR)/apps/sample10/driver.o
 
 
 $(LIBRARY_OBJS): $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp $(INCLUDES) $(DEPS)
@@ -19,14 +18,6 @@ $(BUILD_DIR)/sample%: $(BUILD_DIR)/samples/sample%.o $(LIBRARY) $(DEPS)
 	@mkdir -p $(@D)
 	$(CXXLDV) -o $@ $< $(LDFLAGS)
 
-$(BUILD_DIR)/apps/sample10/driver.o: apps/sample10/driver.cpp $(INCLUDES) $(DEPS)
-	@mkdir -p $(@D)
-	$(CXXV) $(CFLAGS_INTERNAL) $(CFLAGS) $< -o $@ $(INCLUDE_FLAGS) -c
-
-$(BUILD_DIR)/driver: $(BUILD_DIR)/apps/sample10/driver.o $(LIBRARY) $(DEPS)
-	@mkdir -p $(@D)
-	$(CXXLDV) -o $@ $< $(LDFLAGS)
-
 .PHONY: executables
-executables: $(BUILD_DIR)/driver $(SAMPLES) $(SCRIPTS)
+executables: $(SAMPLES)
 
